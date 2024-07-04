4f7139307f6da903b30bd55161a0bf9766a0ceea
Triet Pham <triet.phm@gmail.com>
2024-07-04 14:42:44 +0700

    feat: refactor code and change to official openai lib

--- app.ts
+++ app.ts
@@ -1,31 +1,75 @@
 import dotenv from "dotenv-safe";
-import delay from "delay";
+import OpenAI from "openai";
 import { ChatGPTAPI } from "chatgpt";
-
-dotenv.config();
 
 const { App } = require("@slack/bolt");
 const WAITING_REACTION_EMOJI = "eyes";
 
+dotenv.config();
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // add this
-  appToken: process.env.SLACK_APP_TOKEN, // add this
+  socketMode: true, 
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
-const chatAPI = new ChatGPTAPI({
-  apiKey: process.env.OPENAI_API_KEY,
-  completionParams: {
-    model: "gpt-4o",
-//    temperature: 0.5,
-//    top_p: 0.8,
-  },
+const openai = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], 
 });
 
-// Save conversation id
-let threadMap: Map<string, string> = new Map();
+enum Role {
+  user,
+  assistant
+}
+
+interface ChatMessage {
+  role: string,
+  content: string
+}
+
+async function askChatCompletion(messages) {
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    messages: messages
+  });
+
+  return completion.choices[0]?.message?.content;
+}
+
+function newUserMessage(message: string): ChatMessage {
+  return { role: Role[Role.user], content: message }
+}
+
+function newAssistantMessage(message: string): ChatMessage {
+  return { role: Role[Role.assistant], content: message }
+}
+
+async function reactWaitingEmoji(client, channel, ts) {
+  client.reactions.add({
+      channel: channel,
+      name: WAITING_REACTION_EMOJI,
+      timestamp: ts,
+    });
+}
+
+async function removeWaitingEmoji(client, channel, ts) {
+  client.reactions.remove({
+      channel: channel,
+      name: WAITING_REACTION_EMOJI,
+      timestamp: ts,
+    });
+}
+
+const logWithTimestamp = (message: string): void => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${message}`);
+};
+
+
+// Save all the conversations so we can send this to openai again
+// Map<slack_thread_id, ChatMessage[]>
+let threadMap: Map<string, ChatMessage[]> = new Map();
 
 // --------------------
 
@@ -36,37 +80,39 @@ app.message(async ({ message, say, client, logger }) => {
     const prompt = message.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, "");
 
     // Add a reaction so we know the ChatGPT is replying
-    await client.reactions.add({
-      channel: message.channel,
-      name: WAITING_REACTION_EMOJI,
-      timestamp: message.ts,
-    });
+    await reactWaitingEmoji(client, message.channel, message.ts);
+    logWithTimestamp(`Sent message: ${prompt}`);
 
-    console.log("DM: " + prompt);
+    // Get the conversation for the thread
+    const threadId = message.thread_ts || message.event_ts;
+    let conversations = threadMap.get(threadId) || [];
 
-    const parentMessageId = threadMap.get(message.thread_ts);
-    res = await chatAPI.sendMessage(prompt, {
-      parentMessageId,
-    });
+    // Add the user message to the conversation
+    conversations.push(newUserMessage(prompt));
 
-    response = res.text;
-
-    if (res.id) {
-      const threadId = message.thread_ts || message.event_ts;
-      threadMap.set(threadId, res.id);
+    // Send the conversation to OpenAI
+    let response = await askChatCompletion(conversations);
+    if (!response) {
+      await say({
+        text: "ERROR: Something went wrong, please try again after a while.",
+        thread_ts: message.ts,
+      });
+      return;
     }
+    // Add the response to the conversation
+    conversations.push(newAssistantMessage(response));
+    
+    // Update the threadMap
+    threadMap.set(threadId, conversations);
 
+    // Send response to Slack
     await say({
       text: response,
       thread_ts: message.ts,
     });
 
     // Remove the waiting reaction emoji after response
-    await client.reactions.remove({
-      channel: message.channel,
-      name: WAITING_REACTION_EMOJI,
-      timestamp: message.ts,
-    });
+    await removeWaitingEmoji(client, message.channel, message.ts)
   } catch (err) {
     await say({
       text: "ERROR: Something went wrong, please try again after a while.",
@@ -81,33 +127,43 @@ app.event("app_mention", async ({ event, context, client, say }) => {
   console.log("Mention: " + event.text);
   const prompt = event.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, "");
   try {
+
     // Add a reaction so we know the ChatGPT is replying
-    await client.reactions.add({
-      channel: event.channel,
-      name: WAITING_REACTION_EMOJI,
-      timestamp: event.ts,
-    });
+    await reactWaitingEmoji(client, event.channel, event.ts);
+    logWithTimestamp(`Sent message: ${prompt}`);
 
-    const parentMessageId = threadMap.get(event.thread_ts);
-    res = await chatAPI.sendMessage(prompt, {
-      parentMessageId,
-    });
+    // Get the conversation for the thread
+    const threadId = event.thread_ts || event.event_ts;
+    let conversations = threadMap.get(threadId) || [];
 
-    if (res.id) {
-      const threadId = event.thread_ts || event.event_ts;
-      threadMap.set(threadId, res.id);
+    // Add the user message to the conversation
+    conversations.push(newUserMessage(prompt));
+
+    // Send the conversation to OpenAI
+    let response = await askChatCompletion(conversations);
+    if (!response) {
+      await say({
+        text: "ERROR: Something went wrong, please try again after a while.",
+        thread_ts:event.ts,
+      });
+      return;
     }
+    // Add the response to the conversation
+    conversations.push(newAssistantMessage(response));
 
+    // Update the threadMap
+    threadMap.set(threadId, conversations);
+
+    // Send response to Slack
     await say({
-      text: res.text,
-      thread_ts: event.ts,
+      text: response,
+      thread_ts:event.ts,
     });
+
+
+
     // Remove the waiting reaction emoji after response
-    await client.reactions.remove({
-      channel: event.channel,
-      name: WAITING_REACTION_EMOJI,
-      timestamp: event.ts,
-    });
+    await removeWaitingEmoji(client, event.channel, event.ts)
   } catch (err) {
     await say({
       text: "ERROR: Something went wrong, please try again after a while.",
