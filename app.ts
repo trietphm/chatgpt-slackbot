a3d6d2a24c48e0ffed6d3b9d48ea4dd4425fd9d8
Triet Pham <triet.phm@gmail.com>
2024-07-08 10:08:58 +0700

    feat: support notion integration

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import OpenAI from "openai";
 import slackifyMarkdown from "slackify-markdown";
 
 const { App } = require("@slack/bolt");
+const { Client } = require("@notionhq/client")
+const { NotionToMarkdown } = require("notion-to-md");
 const WAITING_REACTION_EMOJI = "eyes";
 
 dotenv.config();
@@ -18,6 +20,13 @@ const app = new App({
 const openai = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'], 
 });
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+// passing notion client to the option
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
 enum Role {
   user,
@@ -93,9 +102,61 @@ async function fetchMessagesFromSlackThread(client, threadTs, channel) {
   return conversations;
 }
 
+async function getMarkdownDataFromNotionPage(pageId: string) {
+  if (process.env.NOTION_TOKEN == "") {
+    return {success: "false", message: "Notion token is missing"};
+  }
 
+  const mdblocks = await n2m.pageToMarkdown(pageId);
+  const mdString = n2m.toMarkdownString(mdblocks);
 
+  return {success: "true", message: mdString.parent};
+}
 
+async function addNotionPageIntoThread(pageId: string, threadId: string) {
+  const data = await getMarkdownDataFromNotionPage(pageId);
+  if (data.success == "false") {
+    return false;
+  }
+
+  let conversations = threadMap.get(threadId) || [];
+  const prompt = "You are a helpful assistant who answers questions about the following Notion page:\n\n" + data.message;
+
+  // Add the user message to the conversation
+  conversations.push(newUserMessage(prompt));
+  conversations.push(newAssistantMessage("I have read the Notion page content. Ask me any questions about it!"));
+ 
+  // Update the threadMap
+  threadMap.set(threadId, conversations);
+  return true;
+}
+
+function getNotionPageId(message: string): string{
+  const regex = /^<https:\/\/www\.notion\.so\/[^\/]+\/[^\/]+-([0-9a-f]{32})>$/;
+  const match = message.match(regex);
+
+  if (match) {
+      return match[1];
+  } else {
+      // Return null or an appropriate message if no match is found
+      return "";
+  }
+}
+
+async function readNotionPageAndReplySlack(pageId: string, threadId: string, slackTs: any, say: any) {
+  const result = await addNotionPageIntoThread(pageId, threadId);
+  if (!result) {
+    say({
+      text: "ERROR: Something went wrong, please try again after a while.",
+      thread_ts: slackTs,
+    });
+  } else {
+    say({
+      text: "I have read the Notion page content. Ask me any questions about it!",
+      thread_ts: slackTs,
+    });
+  }
+}
 
 
 // --------------------
@@ -110,19 +171,28 @@ let threadMap: Map<string, ChatMessage[]> = new Map();
 app.message(async ({ message, say, client, logger }) => {
   try {
     let prompt = message.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, "");
+    // Get the conversation for the thread
+    const threadId = message.thread_ts || message.event_ts;
 
     // Add a reaction so we know the ChatGPT is replying
     await reactWaitingEmoji(client, message.channel, message.ts);
     logWithTimestamp(`Sent message: ${prompt}`);
 
+    // Check if the message is a Notion page link
+    const notionPageId = getNotionPageId(prompt);
+    if (notionPageId != "") {
+      // Read the notion page content and reply to the slack thread
+      await readNotionPageAndReplySlack(notionPageId, threadId, message.ts, say);
+
+      // Remove the waiting reaction emoji after response
+      return removeWaitingEmoji(client, message.channel, message.ts);
+    }
 
     if (prompt.trim().toLowerCase() == "summary") {
       const SlackThreadMessages = await fetchMessagesFromSlackThread(client, message.thread_ts, message.channel);
       prompt = "Please provide a summary of the following conversation:\n\n" + SlackThreadMessages
     }
 
-    // Get the conversation for the thread
-    const threadId = message.thread_ts || message.event_ts;
     let conversations = threadMap.get(threadId) || [];
 
     // Add the user message to the conversation
@@ -156,6 +226,8 @@ app.message(async ({ message, say, client, logger }) => {
       text: "ERROR: Something went wrong, please try again after a while.",
       thread_ts: message.ts,
     });
+    console.log("Object msg:", message);
+    console.log("Text msg:", message.text);
     console.log(err);
   }
 });
@@ -165,18 +237,28 @@ app.event("app_mention", async ({ event, context, client, say }) => {
   console.log("Mention: " + event.text);
   let prompt = event.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, "");
   try {
+    // Get the conversation for the thread
+    const threadId = event.thread_ts || event.event_ts;
 
     // Add a reaction so we know the ChatGPT is replying
     await reactWaitingEmoji(client, event.channel, event.ts);
     logWithTimestamp(`Sent message: ${prompt}`);
+
+    // Check if the message is a Notion page link
+    const notionPageId = getNotionPageId(prompt);
+    if (notionPageId != "") {
+      // Read the notion page content and reply to the slack thread
+      await readNotionPageAndReplySlack(notionPageId, threadId, event.ts, say);
+
+      // Remove the waiting reaction emoji after response
+      return removeWaitingEmoji(client, event.channel, event.ts);
+    }
 
     if (prompt.trim().toLowerCase() == "summary") {
       const SlackThreadMessages = await fetchMessagesFromSlackThread(client, event.thread_ts, event.channel);
       prompt = "Please provide a summary of the following conversation:\n\n" + SlackThreadMessages
     }
 
-    // Get the conversation for the thread
-    const threadId = event.thread_ts || event.event_ts;
     let conversations = threadMap.get(threadId) || [];
 
     // Add the user message to the conversation
@@ -212,6 +294,9 @@ app.event("app_mention", async ({ event, context, client, say }) => {
       text: "ERROR: Something went wrong, please try again after a while.",
       thread_ts: event.ts,
     });
+    console.log("Object event:", event);
+    console.log("Text event:", event.text);
+
     console.log(err);
   }
 });
